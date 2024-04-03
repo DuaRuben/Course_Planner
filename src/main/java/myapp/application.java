9bac9d8d5fee094690923233d7d4d24338a9141f
DuaRuben <147565092+DuaRuben@users.noreply.github.com>
2024-04-02 23:36:41 -0700

    adasd

--- src/main/java/myapp/application.java
+++ src/main/java/myapp/application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(application.class, args);
     }
 }

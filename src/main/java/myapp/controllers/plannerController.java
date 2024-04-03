2dba62f0e761d871e82b964aa9c6ce2da62e9a9e
DuaRuben <147565092+DuaRuben@users.noreply.github.com>
2024-04-02 23:31:45 -0700

    Update controller

--- src/main/java/myapp/controllers/plannerController.java
+++ src/main/java/myapp/controllers/plannerController.java
@@ -1,11 +1,9 @@
 package myapp.controllers;
 
 import AllApiDtoClasses.ApiAboutDTO;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class plannerController {
     @GetMapping("/api/about")
     public ApiAboutDTO getDescription(){

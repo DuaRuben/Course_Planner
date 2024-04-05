6c57260c8cfdfad74178050eb6b3eedc0a058e6d
DuaRuben <147565092+DuaRuben@users.noreply.github.com>
2024-04-05 16:48:18 -0700

    asad

--- src/main/java/Application/Controller/Controller.java
+++ src/main/java/Application/Controller/Controller.java
@@ -1,9 +1,12 @@
 package Application.Controller;
 
 import AllApiDtoClasses.ApiAboutDTO;
+import AllApiDtoClasses.ApiDepartmentDTO;
 import Application.Model.Manager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class Controller {
@@ -16,4 +19,10 @@ public class Controller {
     public String dumpModel() {
         return manager.printModel();
     }
+
+    @GetMapping("/api/departments")
+    public List<ApiDepartmentDTO> getAllDepartments(){
+        return null;
+    }
+
 }
